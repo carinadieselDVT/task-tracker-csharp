691a90b4a0f586363680142b5dc4f17ee0ec1229
Carina Diesel <cdiesel@dvtsoftware.com>
2026-05-15 11:40:36 +0200

    feat: Added methods and remaining fields

--- obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478baeb4a186e34d38bac6b8c0b1f0f36083f5aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db6435ffae2c335873e9ddbab44c90872b1c3abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
