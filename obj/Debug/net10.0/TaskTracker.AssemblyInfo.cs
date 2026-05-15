0f5021faedcfc258f87a5e933524fd43e9183364
Carina Diesel <cdiesel@dvtsoftware.com>
2026-05-15 13:48:13 +0200

    chore: Added TeamTask logic to Program.cs

--- obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92999f15e2b4de5f331772a598f48603d14b4bea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6267e1e568cf3570a9a6a68c46f263611b5b1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
